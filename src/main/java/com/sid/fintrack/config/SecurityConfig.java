84a6e9c59bf8676d474915bb6aa3e72a11c7c931
Siddharth Sharma <65253410+03sid-sharma@users.noreply.github.com>
2025-07-23 15:08:36 +0530

    feat: middleware jwt

--- src/main/java/com/sid/fintrack/config/SecurityConfig.java
+++ src/main/java/com/sid/fintrack/config/SecurityConfig.java
@@ -1,23 +1,43 @@
 package com.sid.fintrack.config;
 
+import com.sid.fintrack.repository.UserRepository;
+import com.sid.fintrack.security.JWTAuthFilter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+@RequiredArgsConstructor
+@EnableMethodSecurity
 public class SecurityConfig {
 
+    private final JWTAuthFilter jwtAuthFilter;
+
     @Bean
-    SecurityFilterChain springSecurityFilterChain(HttpSecurity http) throws Exception {
+    public UserDetailsService userDetailsService(UserRepository repo) {
+        return username -> repo.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User Not Found!"));
+    }
+
+    @Bean
+    public SecurityFilterChain springSecurityFilterChain(HttpSecurity http) throws Exception {
         return http.authorizeHttpRequests(request->request
-                .requestMatchers("/api/**")
-                .hasRole("CLIENT"))
-                .httpBasic(Customizer.withDefaults())
+                .requestMatchers("/auth/**", "/v3/api-docs/**", "/swagger-ui/**")
+                                .permitAll().anyRequest().authenticated()
+                )
                 .csrf(csrf->csrf.disable())
+                .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 
